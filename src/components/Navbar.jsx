864ad1f6e89d69d4195dd549fa727d4be8606c9e
AmazingAkai <azharmomin420@gmail.com>
2023-07-16 11:42:57 +0530

    add svg to logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,58 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Gift from "../assets/Gift.svg";
 
 const Navbar = () => {
   return (
     <nav className="fixed top-0 w-full bg-gradient-to-r from-indigo-500 via-pink-500 to-purple-500 z-50">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
-        <div className="text-white text-lg font-bold">
-          Giftify
-        </div>
+        <Link to="/" className="text-white text-xl font-bold flex items-center">
+          <img src={Gift} alt="" className="w-8" /> <span className="ml-2 mt-1">Giftify</span>
+        </Link>
         <div className="hidden md:block">
           <ul className="flex space-x-4 text-white">
             <li>
-              <Link to="/" className="hover:text-gray-600 transition-colors duration-500">Home</Link>
+              <Link
+                to="/"
+                className="hover:text-gray-600 transition-colors duration-500"
+              >
+                Home
+              </Link>
             </li>
             <li>
-              <Link to="/documentation" className="hover:text-gray-600 transition-colors duration-500">Documentation</Link>
+              <Link
+                to="/documentation"
+                className="hover:text-gray-600 transition-colors duration-500"
+              >
+                Documentation
+              </Link>
             </li>
             <li>
-              <Link to="/status" className="hover:text-gray-600 transition-colors duration-500">Status</Link>
+              <Link
+                to="/status"
+                className="hover:text-gray-600 transition-colors duration-500"
+              >
+                Status
+              </Link>
             </li>
             <li>
-              <Link to="/invite" className="hover:text-gray-600 transition-colors duration-500">Invite</Link>
+              <Link
+                to="/invite"
+                className="hover:text-gray-600 transition-colors duration-500"
+              >
+                Invite
+              </Link>
             </li>
             <li>
-              <Link to="/support" className="hover:text-gray-600 transition-colors duration-500">Support</Link>
+              <Link
+                to="/support"
+                className="hover:text-gray-600 transition-colors duration-500"
+              >
+                Support
+              </Link>
             </li>
           </ul>
         </div>
-        <div className="md:hidden">
-          {/* MOBILE BUTTON */}
-        </div>
+        <div className="md:hidden">{/* MOBILE BUTTON */}</div>
       </div>
     </nav>
   );
