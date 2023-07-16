dd4c974bbd034c6bbaba1930700d6bc22f1e3f27
AmazingAkai <azharmomin420@gmail.com>
2023-07-16 12:13:11 +0530

    added dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
-// Assets
 import Gift from "../assets/Gift.svg";
 import { Bars3Icon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <nav className="fixed top-0 w-full bg-gradient-to-r from-indigo-500 via-pink-500 to-purple-500 z-50">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
@@ -55,8 +60,56 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <button className="md:hidden"> <Bars3Icon className="w-8 text-white"/> </button>
+        <button className="md:hidden" onClick={toggleDropdown}>
+          <Bars3Icon className="w-8 text-white"/>
+        </button>
       </div>
+      {isDropdownOpen && (
+        <div className="md:hidden absolute left-0 right-0 top-full rounded-b-lg shadow-lg">
+          <ul className="bg-gradient-to-r from-indigo-500 via-pink-500 to-purple-500 text-white py-2 px-4 transition-all duration-500">
+            <li>
+              <Link
+                to="/"
+                className="block py-1 hover:text-gray-800 transition-colors duration-500"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/documentation"
+                className="block py-1 hover:text-gray-800 transition-colors duration-500"
+              >
+                Documentation
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/status"
+                className="block py-1 hover:text-gray-800 transition-colors duration-500"
+              >
+                Status
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/invite"
+                className="block py-1 hover:text-gray-800 transition-colors duration-500"
+              >
+                Invite
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/support"
+                className="block py-1 hover:text-gray-800 transition-colors duration-500"
+              >
+                Support
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
