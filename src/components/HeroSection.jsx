3c22cc83e5db34a27c8f40b3cd4855f175c1ce89
AmazingAkai <azharmomin420@gmail.com>
2023-07-16 13:38:39 +0530

    f

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,7 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 // Assets
-import GiftifyLogo from '../assets/giftify_circle.png';
-
+import GiftifyLogo from "../assets/giftify_circle.png";
 
 const HeroSection = () => {
   return (
@@ -10,9 +9,17 @@ const HeroSection = () => {
       <div className="container mx-auto px-4 md:px-8 py-4 md:py-8 md:pb-16 flex flex-col md:flex-row justify-between items-center md:rounded-lg shadow-lg bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg">
         <div className="text-white text-center md:w-1/2">
           <h1 className="text-4xl font-bold mb-4">Giftify</h1>
-          <p className="text-lg mb-6">An efficient giveaway bot that simplifies hosting highly customizable giveaways with ease.</p>
+          <p className="text-lg mb-6">
+            An efficient giveaway bot that simplifies hosting highly
+            customizable giveaways with ease.
+          </p>
           <div className="flex justify-center">
-            <Link to='/invite' className='btn'>Invite</Link>
+            <Link
+              to="/invite"
+              className="bg-pink-500 hover:bg-pink-400 text-white font-semibold h-12 px-6 md:px-16 rounded-lg w-full flex items-center justify-center sm:w-auto transition-all duration-500 transform hover:scale-110"
+            >
+              Invite
+            </Link>
           </div>
         </div>
         <div className="relative md:mr-5 hidden md:block">
