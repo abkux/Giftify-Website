c8c002ef3c4ba9a864828648b7ee6f96bb371150
Akai <93782497+AmazingAkai@users.noreply.github.com>
2023-07-19 13:42:10 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,47 +3,16 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <>
-    <div className="bg-gray-900">
-      <div className="p-4 flex justify-around flex-col items-center md:flex-row">
-          <div className="p-10">
+      <div className="h-0.5 bg-slate-700"></div>
+      <footer className="bg-gray-900 py-4">
+        <div className="container mx-auto px-2 flex justify-between items-center">
+          <div>
           <p className="text-xl font-semibold bg-gradient-to-r from-primary to-danger bg-clip-text text-transparent from-indigo-500 to-rose-500">
               &copy; Giftify
             </p>
             <span className="text-sm text-white font-thin">
             All rights reserved.
             </span>
-          </div>
-          <div className="p-10">
-          <ul className="">
-            <h1 className="text-white hover:text-zinc-200 underline">Important Links</h1>
-              <li>
-                <Link
-                  href="/terms"
-                  className="text-gray-300 text-sm hover:text-white transition-colors duration-300"
-                >
-                  Terms of Service
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/privacy"
-                  className="text-gray-300 text-sm hover:text-white transition-colors duration-300"
-                >
-                  Privacy Policy
-                </Link>
-              </li>
-            </ul>
-          </div>
-      </div>
-
-    </div>
-      {/* <div className="h-0.5 bg-slate-700"></div>
-      <footer className="bg-gray-900 py-4">
-        <div className="container mx-auto px-2 flex justify-between items-center">
-          <div>
-            <p className="text-gray-400">
-              &copy; Giftify. All rights reserved.
-            </p>
           </div>
           <div>
             <ul className="flex space-x-4">
@@ -66,7 +35,7 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-      </footer> */}
+      </footer>
     </>
   );
 };
